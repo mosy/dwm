febb0707ac5f797302a07d751056e1e2004140e1
Fred-Olav <fred-olav@mosdal.org>
2023-02-21 14:35:25 +0100

    ny dwm 6.4 patchet systray og centered master

--- config.h
+++ config.h
@@ -44,11 +44,11 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
